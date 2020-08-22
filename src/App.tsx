1b10cf61a92b5fe017e7a23fdf6dad46c80a9802
dniel <daniel@engfeldt.net>
2020-08-22 12:19:45 +0200

    add react-router and inital pages for home and profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { UserProvider } from './components/UserContextProvider';
-import WelcomeMessage from "./components/WelcomeMessage";
 import AppMenu from './components/AppMenu';
 import Footer from './components/Footer';
-import Typography from '@material-ui/core/Typography';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import { UserProvider } from './components/UserContextProvider';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,30 +35,19 @@ const App: React.FC = () => {
 
   return (
     <UserProvider>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppMenu />        
-        <Container component="main" className={classes.main} maxWidth="md">
-          <WelcomeMessage/>
-          <Typography paragraph variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Typography>
-          <Typography paragraph variant="body1">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-            quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-            dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
-            voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-          </Typography>
-        </Container>
-        <Footer />
-      </div>
+      <Router>
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppMenu />
+          <Container component="main" className={classes.main} maxWidth="md">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/profile" component={Profile} />
+            </Switch>
+          </Container>
+          <Footer />
+        </div>
+      </Router>
     </UserProvider>
   );
 };
