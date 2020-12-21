e0119587caee3879eb5eac320501d5972e8154ce
dniel <daniel@engfeldt.net>
2020-12-21 11:39:18 +0100

    update regexp to find correct env and add test env to environment check.

--- src/utils/config-utils.ts
+++ src/utils/config-utils.ts
@@ -1,6 +1,7 @@
 export enum Environment {
   LOCAL = "LOCAL",
   DEV = "DEV",
+  TEST = "TEST",
   STAGING = "STAGING",
   PROD = "PROD",
 }
@@ -8,7 +9,7 @@ export enum Environment {
 export interface IAppConfiguration {
   apiUrl: string;
   environment: Environment;
-  authBaseUrl: string
+  authBaseUrl: string;
 }
 
 type IEnvToConfigMap = {
@@ -19,31 +20,38 @@ const envToConfigMap: IEnvToConfigMap = {
   [Environment.LOCAL]: {
     apiUrl: "https://demo.example.test",
     environment: Environment.LOCAL,
-    authBaseUrl: "https://auth.example.test"
-  },
-  [Environment.DEV]: {
-    apiUrl: "",
-    environment: Environment.DEV,
     authBaseUrl: "https://auth.example.test",
   },
-  [Environment.STAGING]: {
-    apiUrl: "",
+  [Environment.DEV]: {
+    apiUrl: "https://demo.example.test",
+    environment: Environment.DEV,
+    authBaseUrl: "https://auth.dev.dniel.in",
+  },
+  [Environment.TEST]: {
+    apiUrl: "https://demo.example.test",
     environment: Environment.STAGING,
-    authBaseUrl: "https://auth.dniel.in",
+    authBaseUrl: "https://auth.test.dniel.in",
+  },
+  [Environment.STAGING]: {
+    apiUrl: "https://demo.example.test",
+    environment: Environment.STAGING,
+    authBaseUrl: "https://auth.stage.dniel.se",
   },
   [Environment.PROD]: {
-    apiUrl: "",
+    apiUrl: "https://demo.example.test",
     environment: Environment.PROD,
-    authBaseUrl: "https://auth.dniel.se",
+    authBaseUrl: "https://auth.prod.dniel.se",
   },
 };
 
 export const getEnvironment = (origin: string): Environment => {
-  if (/^https:\/\/example.test/.test(origin)) {
+  if (/^https?:\/\/(\w*).dev.dniel.[a-z]{2,3}/.test(origin)) {
     return Environment.DEV;
-  } else if (/^https:\/\/dniel.in/.test(origin)) {
+  } else if (/^https?:\/\/(\w*).test.dniel.[a-z]{2,3}/.test(origin)) {
+    return Environment.TEST;
+  } else if (/^https?:\/\/(\w*).stage.dniel.[a-z]{2,3}/.test(origin)) {
     return Environment.STAGING;
-  } else if (/^https:\/\/dniel.se/.test(origin)) {
+  } else if (/^https?:\/\/(\w*).prod.dniel.[a-z]{2,3}/.test(origin)) {
     return Environment.PROD;
   }
 
